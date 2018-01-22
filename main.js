069a9d4ea88783d17f61235cf35659b865facce7
j9recurses <janine.heiser@gmail.com>
2018-01-22 12:02:15 -0800

    adds test email

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const readYaml = require('read-yaml')
 const rmdirRecursiveSync = require('rmdir-recursive').sync
 const Cpass = require('cpass').Cpass
 const cpass = new Cpass()
+const email   = require("emailjs/email");
 
 function readConfigs (fn) {
   return readYaml.sync(fn)
@@ -11,6 +12,8 @@ function readConfigs (fn) {
 const configsFn = './configs/secured_config.yaml'
 const configs = readConfigs(configsFn)
 
+const emailConfigFn './configs/email_config_server.yaml'
+const emailConfigs = readConfigs(emailConfigFn)
 // remove all files in the directory before downloading
 
 const directory = configs.dlRootFolder
@@ -27,9 +30,22 @@ const context = {
   siteUrl: configs.siteUrl,
   creds: {
     username: configs.username,
-    password: cpass.decode(configs.password)
+    password: cpass.decode(configs.password) + "blahblahblahhhhhhhh"
   }
 }
+
+let emailServer = email.server.connect({
+   host:   emailConfigs.server_addr,
+   port: emailConfigs.server_port,
+});
+
+const emailServerFailureMsg = {
+   text:    emailConfigs.etl_failure_msg, 
+   from:    emaillConfigs.etl_sender_addr, 
+   to:      emailConfigs.etl_recipients
+   subject: emailConfigs.etl_failure_subject
+}
+ 
 
 const options = {
   // root remote folder
@@ -53,4 +69,7 @@ sppull(context, options)
   })
   .catch(function (err) {
     console.log('Core error has happened', err)
+    // send the message and get a callback with an error or details of the message that was sent
+    emailServer.send( emailServerFailureMsg, function(err, message) { console.log(err || message); });
+
   })
