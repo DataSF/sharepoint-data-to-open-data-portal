6c06dad0652851b5a81fd4c41d1f6c1f1d28131d
j9recurses <janine.heiser@gmail.com>
2018-01-22 11:15:03 -0800

    adds test email

---
+++ test.js
@@ -0,0 +1,15 @@
+var email 	= require("emailjs/email");
+var server 	= email.server.connect({
+   host:    "10.1.3.216", 
+   port: 25
+   ssl:     false
+});
+ 
+// send the message and get a callback with an error or details of the message that was sent
+server.send({
+   text:    "i hope this works", 
+   from:    "you <username@your-email.com>", 
+   to:      "janine.heiser@sfgov.org",
+   cc:      "else <else@your-email.com>",
+   subject: "testing emailjs"
+}, function(err, message) { console.log(err || message); });
