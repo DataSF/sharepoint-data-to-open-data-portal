87e54908cdfc4aceababdfc898918cd3680070d6
j9recurses <janine.heiser@gmail.com>
2018-01-22 12:54:33 -0800

    adds test email

--- main.js
+++ main.js
@@ -40,10 +40,11 @@ let emailServer = email.server.connect({
 });
 
 const emailServerFailureMsg = {
-   text:    emailConfigs.etl_failure_msg, 
+   // text:    emailConfigs.etl_failure_msg, 
    from:    emailConfigs.etl_sender_addr, 
    to:      emailConfigs.etl_recipients,
    subject: emailConfigs.etl_failure_subject,
+   //attachment:  emailConfigs.etl_failure_msg}]
 }
  
 
@@ -70,7 +71,7 @@ sppull(context, options)
   .catch(function (err) {
     console.log('Core error has happened', err)
     console.log(err)
-    emailServerFailureMsg.text = emailServerFailureMsg.text + "\n\n" + JSON.stringify(err)
+    emailServerFailureMsg.attachment = [{data:"<html><h1>" + emailConfigs.etl_failure_msg + "<h1><br><br>" + JSON.stringify(err) + "</html>", alternative:true}]
     // send the message and get a callback with an error or details of the message that was sent
     emailServer.send( emailServerFailureMsg, function(err, message) { console.log(err || message); });
 
